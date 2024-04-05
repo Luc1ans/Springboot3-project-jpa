5e61f36c32cf4cffbbc6ba37d72a1a106df97a18
Luciano David <davidc.amoura@gmail.com>
2024-04-05 08:18:23 -0300

    Service layer, component registration

---
+++ src/main/java/com/MundoDesign/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.MundoDesign.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.MundoDesign.course.entities.User;
+import com.MundoDesign.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		 Optional<User> obj = repository.findById(id);
+		 return obj.get();
+	}
+}
