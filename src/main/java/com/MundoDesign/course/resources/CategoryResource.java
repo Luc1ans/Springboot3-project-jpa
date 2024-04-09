b41e4f271ae074f77c11e7b2344fc3474cd0efb0
Luciano David <davidc.amoura@gmail.com>
2024-04-09 18:57:59 -0300

    many-to-many association with JoinTable

--- src/main/java/com/MundoDesign/course/resources/CategoryResource.java
+++ src/main/java/com/MundoDesign/course/resources/CategoryResource.java
@@ -13,7 +13,7 @@ import com.MundoDesign.course.entities.Category;
 import com.MundoDesign.course.services.CategoryService;
 
 @RestController
-@RequestMapping(value = "/categorys")
+@RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@Autowired
